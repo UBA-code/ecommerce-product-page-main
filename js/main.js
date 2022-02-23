c4f1cdaabe521d44770b702f8b08621b25e980eb
UBA-code <belhachmiyassine4@gmail.com>
2022-02-23 22:06:51 +0100

    complete developing

--- js/main.js
+++ js/main.js
@@ -27,3 +27,50 @@ navLayer.addEventListener("click", (e) => {
     nav.classList.remove("active");
   }
 });
+
+// start mobile slider
+
+let mobileSlider = document.querySelector(".mobile-silder"),
+  mobileSliderLength =
+    document.querySelectorAll(".preview-img .img-box").length - 1,
+  previewImgMobile = mobileSlider.querySelector(".preview-img"),
+  translateNumber = 0;
+
+mobileSlider.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("next-btn") ||
+    e.target.parentElement.classList.contains("next-btn")
+  ) {
+    if (mobileSliderLength > 0) {
+      mobileSliderLength--;
+      translateNumber += 100;
+      previewImgMobile.style.transform = `translateX(-${translateNumber}%)`;
+    }
+  }
+  if (
+    e.target.classList.contains("previous-btn") ||
+    e.target.parentElement.classList.contains("previous-btn")
+  ) {
+    if (
+      mobileSliderLength <
+      document.querySelectorAll(".preview-img .img-box").length - 1
+    ) {
+      mobileSliderLength++;
+      translateNumber -= 100;
+      previewImgMobile.style.transform = `translateX(-${translateNumber}%)`;
+    }
+  }
+});
+
+// show selected picture
+
+let previewImgDesk = document.querySelector(".slider-area .preview-img"),
+  sliderImagesDesk = document.querySelectorAll(".images-container .img-box:not(.selected-image)");
+
+sliderImagesDesk.forEach((e) => {
+  e.addEventListener("click", (x) => {
+    previewImgDesk.querySelector("img").src = x.target.dataset.src;
+    sliderImagesDesk.forEach(z=> z.classList.remove("slected-image"))
+    e.classList.add("slected-image")
+  });
+});
